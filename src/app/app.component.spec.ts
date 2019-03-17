6ea7e8a75141c25625d1f347b503b43fddf972ae
R Richards <rdrrichards@hotmail.com>
2019-03-17 15:11:41 -0500

    unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,14 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, SidebarModule, ButtonModule
       ],
       declarations: [
         AppComponent
