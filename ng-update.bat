6131e25a2d228e21c2836f314aa6113db379b292
Richard Richards <rdrrichards@hotmail.com>
2019-11-16 06:11:55 -0600

    post update commit

--- ng-update.bat
+++ ng-update.bat
@@ -4,4 +4,5 @@ CALL git commit -am "cli update to latest"
 CALL ng update @angular/core --force
 CALL git commit -am "ng update to latest"
 CALL npm audit fix
+CALL git commit -am "post update commit"
 CALL bt.bat
